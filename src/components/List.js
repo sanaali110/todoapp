42d3f2e3c9a97ff96d989550a5fe27cafa056c88
shinehope <sana_tsm@yahoo.com>
2025-01-24 20:26:34 +0000

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -11,8 +11,19 @@ function List() {
     }
     function handleClick(e){
      e.preventDefault()
-     setTodo([...todos, inputValue])
+    if(inputValue.trim() !== ""){
+      setTodo([...todos, inputValue])
+    }
+     //...todos: The spread operator (...) creates a new 
+     // array that includes all the current items in the
+     //  todos array. It is used to ensure the previous items are preserved when adding a new item.
      setInputValue('')
+    }
+
+     function handleDelete(index){
+       const newTodos = [...todos];
+       newTodos.splice(index, 1);
+       setTodo(newTodos);
     }
   return (
     <div>
@@ -20,14 +31,15 @@ function List() {
        <form>
         <input className='input-list' type="text" value={inputValue} onChange={handleChange} placeholder='add item' />
        
-        <button className="submit"  onClick={handleClick}>Add</button>        
+        <button className="submit"  onClick={handleClick}>Add list item</button>     
       </form>
      <h4>List items</h4>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo}>{todo}
-          
+        {todos.map((todo, index) => (
+          <li className='list-item' key={todo}>{todo}
+          <button className='delete-list' onClick={() => handleDelete(index)}>Delete</button>
           </li>
+         
         ))}
       </ul>
     </div>
